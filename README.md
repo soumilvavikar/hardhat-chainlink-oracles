1dab3ec21ff11c9ccb1309ef23d50a00712de0e6
Soumil <soumil.vavikar@gmail.com>
2024-09-30 21:09:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# COnnecting to the Oracles using Hardhat
+
+## Setting up a new Hardhat Project
+
+```shell
+# Initialize the npm project
+npm init
+# Install Hardhat
+npm install --save-dev hardhat
+# Initialize Hardhat project 
+npx hardhat init
+
+npm install --save-dev @nomicfoundation/hardhat-toolbox
+```
+
+Update the `hardhat.config.js` file.
+
+## Install the Chain Link Contracts
+
+```shell
+
+# Install the chainlink contracts
+npm i @chainlink/contracts
+```
+
+## Write the Contract
+
+Refer to `PriceConsumer.sol`, `MOckAggregatorV3.sol`, and `OracleTest.js`.
+
+## Build and Unit Test the Smart Contract
+
+```shell
+# Build a hardhat project 
+npx hardhat compile
+
+# Run tests in the test folder
+npx hardhat test
+```
---
+++ contracts/PriceConsumerV3.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    
+    constructor(address _feedAddress) {
+        priceFeed = AggregatorV3Interface(
+            _feedAddress
+        );
+    }
+
+    /**
+     * Returns the latest price.
+     */
+    function getLatestPrice() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    /**
+     * This function will get the conversion rate
+     */
+    function getConversionRate(uint256 etherAmount) internal view returns (uint256) {
+        int etherPrice = getLatestPrice();
+        // (1e18 * 1e18) / 1e18
+        uint256 etherAmountInUSD = (uint256(etherPrice) * etherAmount) / 1e18;
+        return etherAmountInUSD;
+    }
+
+    /**
+     * This method will help us get the version of the AggregatorV3Interface
+     */
+    function getVersion() internal view returns (uint256) {
+        return priceFeed.version();
+    }
+}
